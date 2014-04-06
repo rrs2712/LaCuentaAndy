4c7ff6cc94bc65e046e78da6070112bfe5a9cc72
Raul RS <rrs2712@gmail.com>
2014-04-06 11:14:02 -0500

    - Mod AndroidManifest.xml: se agrega la actividad del splash - Mod strings.xml: se agregan mas cadenas - Add logo_tsis.png: para pantalla splash - Add fragment-splash: layout del splash - Add Splash.java: contiene el codigo para lanzar el splash y el main

---
+++ LaCuentaAndy/src/com/tsis/lacuenta/andy/bo/Splash.java
@@ -0,0 +1,96 @@
+package com.tsis.lacuenta.andy.bo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.tsis.lacuenta.andy.main.MainActivity;
+import com.tsis.lacuenta.andy.main.R;
+
+public class Splash extends Activity {
+
+	// Splash screen timer
+    private static int SPLASH_TIME_OUT = 1500;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    //Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    //Remove notification bar
+	    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		
+//		setContentView(R.layout.activity_splash);
+		setContentView(R.layout.fragment_splash);
+		
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+				
+			}
+		}, SPLASH_TIME_OUT);
+		
+		
+
+//		if (savedInstanceState == null) {
+//			getSupportFragmentManager().beginTransaction()
+//					.add(R.id.container, new PlaceholderFragment()).commit();
+//		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_splash,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
